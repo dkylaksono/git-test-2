4bcad3d8463fc4e401d0173b4fbbe9b34a18ef55
dkylaksono <ddiicckkyy865@gmail.com>
2023-05-10 11:36:32 +0900

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore facilis excepturi blanditiis reiciendis exercitationem fuga neque nulla libero mollitia officia ipsum, explicabo, totam, tenetur accusamus dolorem adipisci cum ipsam. Quod.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
